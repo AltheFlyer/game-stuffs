132467f926982b9b815fe1c16f92d1c00cf45d99
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2017-08-01 19:40:33 -0400

    Update GameScreen.java
    + Added specific Enemy spawn areas 
    + Added health (player can take 10 bullet hits)
    + Sprites are now actually disposed
    ~ Redordered list of variables
    ~ Modified game area to fit sidebar later
    
    !!! Shooting an enemy with multiple bullets at the same time (Point blank) causes random enemies to despawn

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -20,15 +20,21 @@ public class GameScreen implements Screen{
 	private Texture characterImage;
 	private Texture enemyImage;
 	private Texture bulletImage;
+	private Texture spawnImage;
+	private Texture spawnImage2;
+	private Texture backgroundImage;
 	private OrthographicCamera camera;
 	private Rectangle character;
 	private Array<Enemy> enemies;
-	private long lastSpawn;
-	private long cooldown;
 	private Array<Bullet> bullets;
 	private Vector3 mousePos;
-	private Texture backgroundImage;
 	private int randomValue;
+	private int spawnX;
+	private int spawnY;
+	private int playerHealth;
+	private long lastSpawn;
+	private long cooldown;
+
 	
 	public GameScreen(final ArenaGame game) {
 		this.game = game;
@@ -36,6 +42,8 @@ public class GameScreen implements Screen{
 		enemyImage = new Texture("regular enemy.png");
 		bulletImage = new Texture("FriendlyBullet.png");
 		backgroundImage = new Texture("BG.png");
+		spawnImage = new Texture("spawnArea.png");
+		spawnImage2 = new Texture("spawnArea2.png");
 		
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1000, 600);
@@ -47,6 +55,7 @@ public class GameScreen implements Screen{
 		character.height = 10;
 		
 		cooldown = 0;
+		playerHealth = 100;
 		
 		enemies = new Array<Enemy>();
 		bullets = new Array<Bullet>();
@@ -63,7 +72,7 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
-		System.out.println(Gdx.graphics.getDeltaTime());
+		//System.out.println(Gdx.graphics.getDeltaTime());
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -71,14 +80,24 @@ public class GameScreen implements Screen{
 		
 		game.batch.setProjectionMatrix(camera.combined);
 		
+		//Draw everything
 		game.batch.begin();
+		//Background
 		game.batch.draw(backgroundImage, 0, 0);
+		//Enemy Spawners
+		game.batch.draw(spawnImage, 0, 225);
+		game.batch.draw(spawnImage, 750, 225);
+		game.batch.draw(spawnImage2, 350, 0);
+		game.batch.draw(spawnImage2, 350, 500);
+		//Enemies
 		for(Enemy enemy: enemies){
 			game.batch.draw(enemyImage, enemy.x ,enemy.y);
 		}
+		//Bullets
 		for (Bullet bullet: bullets){
 			game.batch.draw(bulletImage, bullet.x, bullet.y);
 		}
+		//Character
 		game.batch.draw(characterImage, character.x, character.y);
 		game.batch.end();
 		
@@ -101,8 +120,8 @@ public class GameScreen implements Screen{
 		//Keep character onscreen
 		if (character.x < 0) {
 			character.x = 0;
-		} else if (character.x > 990) {
-			character.x = 990;
+		} else if (character.x > 840) {
+			character.x = 840;
 		}
 		if (character.y < 0) {
 			character.y = 0;
@@ -131,7 +150,7 @@ public class GameScreen implements Screen{
 		while(iter1.hasNext()){
 			Bullet bullet = iter1.next();
 			bullet.moveBullet(Gdx.graphics.getDeltaTime());
-			if (bullet.x < 0 || bullet.x > 1000 || bullet.y < 0 || bullet.y > 600) {
+			if (bullet.x < 0 || bullet.x > 850 || bullet.y < 0 || bullet.y > 600) {
 				//Remove if out of bounds
 				iter1.remove();
 			}	
@@ -152,11 +171,17 @@ public class GameScreen implements Screen{
 						iterEnemy.remove();
 					} else if (bullet.hitbox.overlaps(character) && !bullet.friendly) {
 						//Player collisions
-						game.setScreen(new MainMenuScreen(game));
+						playerHealth -= 10;
+						System.out.println(playerHealth);
+						iterBullet.remove();
+						
+						if (playerHealth <= 0) {
+							game.setScreen(new MainMenuScreen(game));
+						}
 					}
 				}
 			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("why");
+				System.out.println("Something went wrong with the arrays");
 			}
 		}
 		
@@ -206,20 +231,48 @@ public class GameScreen implements Screen{
 		characterImage.dispose();
 		enemyImage.dispose();
 		bulletImage.dispose();
+		spawnImage.dispose();
+		spawnImage2.dispose();
+		backgroundImage.dispose();
 	}
 	
 	//Generate a random enemy
 	private void spawnEnemy() {
+		//Generate spawn area first
+		randomValue = MathUtils.random(1, 4);
+		switch (randomValue) {
+			case 1: 
+				//Left spawner
+				spawnX = MathUtils.random(10, 90);
+				spawnY = MathUtils.random(235, 365);
+				break;
+			case 2:
+				//Top spawner
+				spawnX = MathUtils.random(360, 490);
+				spawnY = MathUtils.random(510, 590);
+				break;
+			case 3:
+				//Right spawner
+				spawnX = MathUtils.random(750, 840);
+				spawnY = MathUtils.random(235, 365);
+				break;
+			case 4:
+				//Bottom spawner
+				spawnX = MathUtils.random(360, 490);
+				spawnY = MathUtils.random(10, 90);
+				break;
+		}
+				
 		randomValue = MathUtils.random(1, 3);
 		
 		switch (randomValue) {
-			case 1: RegularEnemy enemy = new RegularEnemy(MathUtils.random(0, 987), MathUtils.random(0, 587), 0, 0, 0, 13, 13, 100);
+			case 1: RegularEnemy enemy = new RegularEnemy(spawnX, spawnY, 0, 0, 0, 13, 13, 100);
 					enemies.add(enemy);
 					break;
-			case 2: TripleShot tri = new TripleShot(MathUtils.random(0, 987), MathUtils.random(0, 587), 0, 0, 0, 13, 13, 0);
+			case 2: TripleShot tri = new TripleShot(spawnX, spawnY, 0, 0, 0, 13, 13, 0);
 					enemies.add(tri);
 					break;
-			case 3: MachineGun mach = new MachineGun(MathUtils.random(0, 987), MathUtils.random(0, 587), 0, 0, 0, 13, 13, 0);
+			case 3: MachineGun mach = new MachineGun(spawnX, spawnY, 0, 0, 0, 13, 13, 0);
 					enemies.add(mach);
 					break;
 		}
