a60402f319dde5ce9b44409d3067faf7fd3ce4e2
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2017-08-07 14:36:09 -0400

    Update ArenaGame.java
    Added commented code for bitmap fonts

--- core/src/com/mygdx/game/ArenaGame.java
+++ core/src/com/mygdx/game/ArenaGame.java
@@ -2,16 +2,19 @@ package com.mygdx.game;
 
 
 import com.badlogic.gdx.Game;
+//import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 public class ArenaGame extends Game {
 	
 	public SpriteBatch batch;
+	//public BitmapFont font;
 	
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
+		//font = new BitmapFont();
 		this.setScreen(new MainMenuScreen(this));
 	}
 
@@ -23,6 +26,7 @@ public class ArenaGame extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
+		//font.dispose();
 	}
 	
 	
