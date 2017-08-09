aefe9f486a8f4c49d2333889a5dbaf9e42662207
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2017-08-09 12:42:21 -0400

    Update TripleShot.java
    + Implemented enemy health

--- core/src/com/mygdx/game/TripleShot.java
+++ core/src/com/mygdx/game/TripleShot.java
@@ -8,7 +8,7 @@ public class TripleShot extends Enemy {
 	
 	public TripleShot(float startX, float startY, float toX, float toY, int hp, int height,
 			int width, int moved) {
-		super(startX, startY, toX, toY, hp, height, width, moved, 3);
+		super(startX, startY, toX, toY, 70, height, width, moved, 3);
 		// TODO Auto-generated constructor stub
 	}
 	
